3833bbbed99d7950bf60c903bbd62ffc2c01115c
hwen <hwenleung@gmail.com>
2015-11-18 17:42:20 +0800

    orz...

---
+++ js/home.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	var showPost = function(post){
+		console.log(post);
+		var contentBox = document.querySelector('.content-box');
+		post.forEach(function(item,index,array){
+			var orgBox = contentBox.cloneNode(true);
+			orgBox.querySelector('.avatar').setAttribute('src',item.user.avatar_hd);
+			orgBox.querySelector('.user-name').innerHTML=item.user.name;
+			orgBox.querySelector('.source').innerHTML = item.source;
+			orgBox.querySelector('.default-content').innerHTML = item.text;
+			//orgBox.querySelector('.weibo-orignal').innerHTML = item.retweeted_status==null ? "":item.retweeted_status.text;
+			orgBox.style.display = '';
+			var sm = document.querySelector('.content-box-container').appendChild(orgBox);
+		});
+	};
+
+	var getPost = function(){
+		var api = "https://api.weibo.com/2/statuses/friends_timeline.json?access_token=2.00lYYxNEGUv6HEbba92b7e5cWAw5sD&count=2";
+		$.ajax({
+			url:api,
+			type:'get',
+			success:function(data){
+				var data = data.statuses;
+				showPost(data);
+			}
+		});
+	};
+
+	getPost();
+});
